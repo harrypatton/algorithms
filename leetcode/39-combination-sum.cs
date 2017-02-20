1d7c5c7056983f54cbc8e8c1b8ace0ba724d1561
harrypatton <bohuang@outlook.com>
2017-02-20 12:48:53 -0800

    Update 39-combination-sum.cs

--- leetcode/39-combination-sum.cs
+++ leetcode/39-combination-sum.cs
@@ -1,21 +1,16 @@
-    public class Solution
-    {
-        public IList<IList<int>> CombinationSum(int[] candidates, int target)
-        {
+    public class Solution {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target) {
             return CombinationSum(candidates, 0, target);
         }
 
-        public IList<IList<int>> CombinationSum(int[] candidates, int startIndex, int target)
-        {
+        public IList<IList<int>> CombinationSum(int[] candidates, int startIndex, int target) {
             var result = new List<IList<int>>();
-            if (candidates == null || startIndex > candidates.Length - 1 || target < 0)
-            {
+            if (candidates == null || startIndex > candidates.Length - 1 || target < 0) {
                 return result;
             }
 
             // current element is the candidate
-            if (target == candidates[startIndex])
-            {
+            if (target == candidates[startIndex]) {
                 result.Add(new List<int>(new int[] { candidates[startIndex] }));
             }
 
@@ -24,8 +19,7 @@
 
             // result including first element
             var subResult = CombinationSum(candidates, startIndex + 1, target - candidates[startIndex]);
-            foreach (var list in subResult)
-            {
+            foreach (var list in subResult) {
                 list.Insert(0, candidates[startIndex]);
             }
 
