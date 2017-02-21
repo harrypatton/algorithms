79e74ff44f5ad88677043c3f550adc65ec9641c9
harrypatton <bohuang@outlook.com>
2017-02-21 15:35:54 -0800

    Update lc90-subsets-II.cs

--- interview/subset/lc90-subsets-II.cs
+++ interview/subset/lc90-subsets-II.cs
@@ -38,3 +38,47 @@ public class Solution_backtracking {
         }
     }
 }
+
+public class Solution_Iteration {
+    public IList<IList<int>> SubsetsWithDup(int[] nums) {
+        var result = new List<IList<int>>();
+        
+        if (nums == null) {
+            return result;
+        }
+        
+        Array.Sort(nums);
+        
+        int index = 0;
+        result.Add(new List<int>());
+        
+        while(index < nums.Length) {
+            int count = 1;
+            
+            // find current repetitive numbers
+            while (index < nums.Length - 1 && nums[index] == nums[index+1]) {
+                count++;
+                index++;
+            }
+            
+            var list = new List<int>();
+            int length = result.Count;
+            
+            for(int i = 0; i < count; i++) {
+                list.Add(nums[index]);
+                                
+                for(int j = 0; j < length; j++) {
+                    var tempList = new List<int>(list);
+                    tempList.AddRange(result[j]);
+                    result.Add(tempList);
+                }
+            }
+            
+            // when the inner while quit, the index is either at the end of repetitive char
+            // or reach the last element. we need to increase it for next loop.
+            index++;
+        }
+        
+        return result;
+    }
+}
