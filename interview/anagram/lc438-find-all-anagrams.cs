0262b95b95876818d8a2bca24ef8e56a5dd5300d
harrypatton <bohuang@outlook.com>
2017-03-01 16:24:16 -0800

    Update lc438-find-all-anagrams.cs

--- interview/anagram/lc438-find-all-anagrams.cs
+++ interview/anagram/lc438-find-all-anagrams.cs
@@ -1,6 +1,50 @@
 public class Solution {
     public IList<int> FindAnagrams(string s, string p) {
         var result = new List<int>();
+        if (s == null || p == null || p.Length > s.Length) {
+            return result;
+        }
+        
+        int start = 0;
+        int end = 0;
+        int count = p.Length;
+        var charCount = new int[26];
+        
+        foreach (var c in p) {
+            charCount[c - 'a']++;
+        }
+        
+        while (end < s.Length) {
+            if(charCount[s[end] - 'a'] > 0) {
+                count--;
+            }
+            
+            charCount[s[end] - 'a']--;
+            end++;
+            
+            if (count == 0) {
+                result.Add(start);
+            }
+            
+            // we need to move start when time window becomes bigger.
+            if (end - start + 1 > p.Length) {
+                charCount[s[start] - 'a']++;
+                
+                if(charCount[s[start] - 'a'] > 0) {
+                    count++;
+                }
+                
+                start++;
+            }
+        }
+        
+        return result;
+    }
+}
+
+public class Solution_1stSln {
+    public IList<int> FindAnagrams(string s, string p) {
+        var result = new List<int>();
         
         if (s == null || p == null || s.Length < p.Length) {
             return result;
