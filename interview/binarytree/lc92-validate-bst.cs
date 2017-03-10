57d704ad8029d702f6b4d01e88c815bedcfd2a2a
harrypatton <bohuang@outlook.com>
2017-03-10 10:03:49 -0800

    Update lc92-validate-bst.cs

--- interview/binarytree/lc92-validate-bst.cs
+++ interview/binarytree/lc92-validate-bst.cs
@@ -43,3 +43,42 @@ public class Solution_InOrder {
         TraverseBST(node.right);
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution_Recursion {
+    private TreeNode previous = null;
+    private TreeNode current = null;
+    
+    public bool IsValidBST(TreeNode root) {
+        int min, max;
+        return IsValidBST(root, out min, out max);
+    }
+    
+    public bool IsValidBST(TreeNode node, out int min, out int max) {
+        min = 0;
+        max = 0;
+        
+        if (node == null ) {
+            min = int.MinValue;
+            max = int.MaxValue;
+            return true;
+        }
+        
+        int left_min, left_max, right_min, right_max;
+        if (IsValidBST(node.left, out left_min, out left_max) && IsValidBST(node.right, out right_min, out right_max)) {
+            min = node.left == null ? node.val : left_min;
+            max = node.right == null ? node.val : right_max;
+            return (node.left == null || node.val > left_max) && (node.right == null || node.val < right_min);
+        }
+        
+        return false;
+    }
+}
